df7cdbcf5ac6c75597ac813c3a692f72970a0a85
James Kellie <Jpk_GameID@hotmail.com>
2021-12-10 12:30:33 +1100

    More commands
    **Welcome**
    Displays the DM'd welcome message with a short rules, and our recommended tutorial content. (ephemeral)
    
    **Ping**
    Same as !ping, just displays the bots client latency. (ephemeral)

--- DiscordBot/Modules/SlashModule.cs
+++ DiscordBot/Modules/SlashModule.cs
@@ -12,28 +12,35 @@ namespace DiscordBot.Modules
 {
     public class SlashModule : InteractionModuleBase
     {
+        private readonly UserService _userService;
+
         private List<string> userModuleHelp;
         private Embed[] userModuleHelpEmbed;
 
         private List<string> userModuleHelpCommands;
-        public SlashModule( IServiceProvider serviceProvider)
+
+        public SlashModule(IServiceProvider serviceProvider, UserService userService)
         {
+            _userService = userService;
+
             Task.Run(async () =>
             {
                 var commandHandling = serviceProvider.GetService<CommandHandlingService>();
                 if (commandHandling != null)
                 {
-                    var userModuleHelpString = await commandHandling.GetCommandList(nameof(UserModule), includeModuleName: false);
+                    var userModuleHelpString =
+                        await commandHandling.GetCommandList(nameof(UserModule), includeModuleName: false);
                     userModuleHelp = userModuleHelpString.MessageSplitToSize();
-                    
+
                     List<Embed> tempEmbed = new List<Embed>();
-                    foreach (var help in userModuleHelp)    
+                    foreach (var help in userModuleHelp)
                     {
                         EmbedBuilder embed = new EmbedBuilder();
                         embed.WithTitle($"User Commands\t Page #{tempEmbed.Count + 1}");
                         embed.Description = help;
                         tempEmbed.Add(embed.Build());
                     }
+
                     userModuleHelpEmbed = tempEmbed.ToArray();
 
                     userModuleHelpCommands =
@@ -41,8 +48,9 @@ namespace DiscordBot.Modules
                 }
             });
         }
-        
-        [SlashCommand("help", "Show commands to user")]
+
+        #region UserModule Commands
+        [SlashCommand("help", "Shows available commands")]
         private async Task Help(string search = "")
         {
             await Context.Interaction.DeferAsync();
@@ -54,9 +62,13 @@ namespace DiscordBot.Modules
                 foreach (var command in userModuleHelpCommands)
                 {
                     if (command.Contains(search))
+                    {
                         sb.Append(command);
+                        if (sb.Length > 3000)
+                            break;
+                    }
                 }
-                
+
                 if (sb.Length == 0)
                     await Context.Interaction.FollowupAsync("No commands found");
                 else
@@ -68,5 +80,21 @@ namespace DiscordBot.Modules
                 }
             }
         }
+        
+        [SlashCommand("welcome", "An introduction to the server!")]
+        public async Task SlashWelcome()
+        {
+            await Context.Interaction.RespondAsync(string.Empty,
+                embed: _userService.GetWelcomeEmbed(Context.User.Username), ephemeral: true);
+        }
+        
+        [SlashCommand("ping", "Bot latency")]
+        public async Task Ping()
+        {
+            await Context.Interaction.RespondAsync($"Bot latency: ...", ephemeral: true);
+            await Context.Interaction.ModifyOriginalResponseAsync(m =>
+                m.Content = $"Bot latency: {_userService.GetGatewayPing().ToString()}ms");
+        }
+        #endregion
     }
 }
