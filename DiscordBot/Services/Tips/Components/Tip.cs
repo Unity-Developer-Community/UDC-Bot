3cc02e4b6aa78ff9fdc3d0cb1a364887f55f2faa
Ed Halley <1223980+hariedo@users.noreply.github.com>
2025-04-23 18:30:25 -0500

    Merge SimplyJPK's Tip.cs

---
+++ DiscordBot/Services/Tips/Components/Tip.cs
@@ -0,0 +1,8 @@
+namespace DiscordBot.Services.Tips.Components;
+
+public class Tip
+{
+	public string Content { get; set; }
+	public List<string> Keywords { get; set; }
+	public List<string> ImagePaths { get; set; }
+}
