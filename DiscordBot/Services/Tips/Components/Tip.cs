2412d9dedefe31f5e2acb88330c5ece381882ba9
Ed Halley <1223980+hariedo@users.noreply.github.com>
2025-04-24 13:34:28 -0500

    Update Tip.cs
    * Added an Id property for editing/removing tips later.
    
    Without this, you'd have to remove multiple tips based on a keyword search.  With this, however, we now have to show the Id property in any editing interfaces.  It's up to the service how to choose values for Id.

--- DiscordBot/Services/Tips/Components/Tip.cs
+++ DiscordBot/Services/Tips/Components/Tip.cs
@@ -1,7 +1,8 @@
 namespace DiscordBot.Services.Tips.Components;
 
-public class Tip
+public class Tip: IEntity<ulong>
 {
+	public ulong Id { get; set; }
 	public string Content { get; set; }
 	public List<string> Keywords { get; set; }
 	public List<string> ImagePaths { get; set; }
